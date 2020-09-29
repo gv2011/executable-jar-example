c7c370cac5b210689c59383a1ff16e7eb9fa731b
Vinz <gv2011@users.noreply.github.com>
2020-09-29 12:31:22 +0200

    Initial commit

---
+++ src/main/java/com/github/gv2011/exjar/Main.java
@@ -0,0 +1,15 @@
+package com.github.gv2011.exjar;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+public final class Main {
+	
+	private static final Logger LOG = getLogger(Main.class);
+	
+	public static void main(String[] args){
+		LOG.info("¡Hola!");
+	}
+
+}
